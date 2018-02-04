a3df575bf4db808dbffed07aba3cfe343d42797f
Niki Kokkinos <Niki@Nikis-MacBook-Air.local>
2018-02-04 13:31:12 -0500

    New Bronx Site

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,1 @@
 
-// header color change
-$('.Recipesbutton').click(function() {
-  $('#header').css('background', getRandomColor())
-})
-
-function getRandomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
