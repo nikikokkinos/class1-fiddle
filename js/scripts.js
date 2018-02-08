0083251b7221ddb57a14d0b63f2eaabdedad31fd
Niki Kokkinos <Niki@Nikis-MacBook-Air.local>
2018-02-08 16:55:57 -0500

    css background

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,10 @@
-<script
-  src="http://code.jquery.com/jquery-3.3.1.min.js"
-  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-  crossorigin="anonymous"></script>
-  
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js">
+</script>
+
 <script>
 $(document).ready(function(){
     $("button").click(function(){
-        var imgUrl = $(this).data('rel');
-        $("#area").html("<img src='" + imgUrl + "' alt='description' />");
-    });
+      $(this).css("background-color", "#ffffff");
 });
 </script>
